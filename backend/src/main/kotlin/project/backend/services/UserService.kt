6d4bdc30e8438a64d376ea55af1cef59745e1abc
davidalexandru1370 <fanefaraon696@gmail.com>
2023-07-30 12:53:49 +0300

    ADDED: User service interface and concrete class

---
+++ backend/src/main/kotlin/project/backend/services/UserService.kt
@@ -0,0 +1,17 @@
+package project.backend.services
+
+import org.springframework.stereotype.Service
+import project.backend.domain.AuthResult
+import project.backend.domain.UserCredentials
+import project.backend.services.interfaces.IUserService
+
+@Service
+class UserService : IUserService {
+    override fun login(userCredentials: UserCredentials): AuthResult {
+        TODO("Not yet implemented")
+    }
+
+    override fun register(userCredentials: UserCredentials): AuthResult {
+        TODO("Not yet implemented")
+    }
+}
