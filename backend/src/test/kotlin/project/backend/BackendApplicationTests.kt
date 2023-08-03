3edf329cddea3571846b35a761cb51747eb934a3
davidalexandru1370 <fanefaraon696@gmail.com>
2023-08-03 22:56:06 +0300

    fix: removed spring boot test annotation in test file

--- backend/src/test/kotlin/project/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/project/backend/BackendApplicationTests.kt
@@ -2,8 +2,14 @@ package project.backend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.context.support.AnnotationConfigContextLoader
 
-@SpringBootTest
+@TestPropertySource(properties = [
+	"spring.jpa.hibernate.ddl-auto=none",
+	"spring.datasource.url="
+])
 class BackendApplicationTests {
 
 	@Test
