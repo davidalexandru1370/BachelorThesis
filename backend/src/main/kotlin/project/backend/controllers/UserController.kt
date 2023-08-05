996dfb880f89cecba1b94628a514690ba0c2df7a
davidalexandru1370 <fanefaraon696@gmail.com>
2023-08-05 23:35:30 +0300

    added(controllers.UserController): iendpoint for login

--- backend/src/main/kotlin/project/backend/controllers/UserController.kt
+++ backend/src/main/kotlin/project/backend/controllers/UserController.kt
@@ -11,21 +11,34 @@ import org.springframework.web.bind.annotation.RestController
 import project.backend.domain.AuthResult
 import project.backend.domain.UserCredentials
 import project.backend.services.UserService
+import project.backend.services.interfaces.IUserService
 
 @RestController
 @RequestMapping(path = ["/api/user"])
 class UserController(
-   @Autowired private val userService: UserService
+
+    @Autowired private val userService: IUserService
 ) {
 
-   @PostMapping(path = ["/register"])
-   fun register(@Valid @RequestBody userCredentials: UserCredentials): ResponseEntity<AuthResult> {
-      val isRegistered = userService.register(userCredentials)
-      return if (isRegistered.result){
-         ResponseEntity.ok(isRegistered)
-      } else{
-         ResponseEntity(isRegistered, HttpStatus.BAD_REQUEST)
-      }
-   }
+    @PostMapping(path = ["/login"])
+    fun login(@Valid @RequestBody userCredentials: UserCredentials): ResponseEntity<AuthResult> {
+        val loginResult: AuthResult = userService.login(userCredentials)
+        return if (loginResult.result) {
+            ResponseEntity.ok(loginResult)
+        } else {
+            ResponseEntity(loginResult, HttpStatus.BAD_REQUEST)
+        }
+    }
+
+
+    @PostMapping(path = ["/register"])
+    fun register(@Valid @RequestBody userCredentials: UserCredentials): ResponseEntity<AuthResult> {
+        val isRegistered = userService.register(userCredentials)
+        return if (isRegistered.result) {
+            ResponseEntity.ok(isRegistered)
+        } else {
+            ResponseEntity(isRegistered, HttpStatus.BAD_REQUEST)
+        }
+    }
 
 }
