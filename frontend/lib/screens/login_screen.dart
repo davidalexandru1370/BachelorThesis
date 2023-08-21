0d6215369307360cdafc14eb6404197f32dee2da
davidalexandru1370 <fanefaraon696@gmail.com>
2023-08-21 21:08:47 +0300

    fix: renderflex overflow

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -17,125 +17,137 @@ class _LoginScreenState extends State<LoginScreen> {
     final passwordController = TextEditingController();
 
     return Scaffold(
-        body:
-            Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-      const Padding(
-        padding: EdgeInsets.only(left: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+      resizeToAvoidBottomInset: false,
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Row(
-              children: [
-                Text(
-                  "Don't have an ",
-                  style: TextStyle(
-                      fontFamily: "PTSansNarrow",
-                      fontSize: 30,
-                      color: Color.fromARGB(255, 43, 43, 43)),
-                ),
-                Text(
-                  "account?",
-                  style: TextStyle(
-                    fontFamily: "PTSansNarrow",
-                    fontSize: 30,
-                    color: Color.fromARGB(255, 39, 33, 234),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Text("Create account",
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                        fontFamily: "BricolageGrotesque",
-                        fontSize: 20,
-                        color: Color.fromARGB(255, 43, 43, 43))),
-              ],
-            )
-          ],
-        ),
-      ),
-      Form(
-          key: formKey,
-          child: Container(
-            alignment: Alignment.center,
-            child:
-                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              const Padding(padding: EdgeInsets.all(5)),
-              FractionallySizedBox(
-                  widthFactor: 0.9,
-                  child: TextFormField(
-                    controller: emailController,
-                    decoration: const InputDecoration(
-                      suffixIcon: Icon(Icons.email),
-                      labelText: "Email",
-                      labelStyle:
-                          TextStyle(color: Color.fromARGB(255, 43, 43, 43)),
-                    ),
-                  )),
-              const Padding(padding: EdgeInsets.all(5)),
-              FractionallySizedBox(
-                  widthFactor: 0.9,
-                  child: TextFormField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: const InputDecoration(
-                      suffixIcon: Icon(Icons.lock),
-                      border: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Color.fromARGB(255, 212, 212, 212))),
-                      labelText: "Password",
-                      labelStyle: TextStyle(
-                        color: Color.fromARGB(255, 43, 43, 43),
-                      ),
-                    ),
-                  )),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.end,
+            const Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Padding(
-                    padding: EdgeInsets.only(right: 20, top: 10),
-                    child: Text(
-                      "Forgot password",
-                      textAlign: TextAlign.right,
-                    ),
+                  Row(
+                    children: [
+                      Text(
+                        "Don't have an ",
+                        style: TextStyle(
+                            fontFamily: "PTSansNarrow",
+                            fontSize: 30,
+                            color: Color.fromARGB(255, 43, 43, 43)),
+                      ),
+                      Text(
+                        "account?",
+                        style: TextStyle(
+                          fontFamily: "PTSansNarrow",
+                          fontSize: 30,
+                          color: Color.fromARGB(255, 39, 33, 234),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text("Create account",
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontFamily: "BricolageGrotesque",
+                              fontSize: 20,
+                              color: Color.fromARGB(255, 43, 43, 43))),
+                    ],
                   )
                 ],
               ),
-              const Padding(padding: EdgeInsets.all(5)),
-              Container(
-                  decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      gradient: LinearGradient(colors: [
-                        Color.fromARGB(255, 25, 77, 221),
-                        Color.fromARGB(255, 45, 3, 171),
-                      ])),
-                  child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(0, 52),
-                        elevation: 0,
-                        shape: const CircleBorder(eccentricity: 0),
-                        shadowColor: Colors.transparent,
-                        backgroundColor: Colors.transparent,
-                      ),
-                      child: const Icon(
-                        color: Colors.white,
-                        Icons.arrow_right_alt_rounded,
-                        size: 40,
-                      ))),
-              const Padding(padding: EdgeInsets.all(10)),
-              const Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  LoginWithGoogleButton(),
-                  Padding(padding: EdgeInsets.all(10)),
-                  LoginWithFacebookButton()
-                ],
-              ),
-            ]),
-          )),
-    ]));
+            ),
+            Form(
+                key: formKey,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Padding(padding: EdgeInsets.all(5)),
+                        FractionallySizedBox(
+                            widthFactor: 0.9,
+                            child: TextFormField(
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return "Please enter your email";
+                                }
+                                return "";
+                              },
+                              controller: emailController,
+                              decoration: const InputDecoration(
+                                suffixIcon: Icon(Icons.email),
+                                labelText: "Email",
+                                labelStyle: TextStyle(
+                                    color: Color.fromARGB(255, 43, 43, 43)),
+                              ),
+                            )),
+                        const Padding(padding: EdgeInsets.all(5)),
+                        FractionallySizedBox(
+                            widthFactor: 0.9,
+                            child: TextFormField(
+                              obscureText: true,
+                              controller: passwordController,
+                              decoration: const InputDecoration(
+                                suffixIcon: Icon(Icons.lock),
+                                border: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                        color: Color.fromARGB(
+                                            255, 212, 212, 212))),
+                                labelText: "Password",
+                                labelStyle: TextStyle(
+                                  color: Color.fromARGB(255, 43, 43, 43),
+                                ),
+                              ),
+                            )),
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(right: 20, top: 10),
+                              child: Text(
+                                "Forgot password",
+                                textAlign: TextAlign.right,
+                              ),
+                            )
+                          ],
+                        ),
+                        const Padding(padding: EdgeInsets.all(5)),
+                        Container(
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                gradient: LinearGradient(colors: [
+                                  Color.fromARGB(255, 25, 77, 221),
+                                  Color.fromARGB(255, 45, 3, 171),
+                                ])),
+                            child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(0, 52),
+                                  elevation: 0,
+                                  shape: const CircleBorder(eccentricity: 0),
+                                  shadowColor: Colors.transparent,
+                                  backgroundColor: Colors.transparent,
+                                ),
+                                child: const Icon(
+                                  color: Colors.white,
+                                  Icons.arrow_right_alt_rounded,
+                                  size: 40,
+                                ))),
+                        const Padding(padding: EdgeInsets.all(10)),
+                        const Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            LoginWithGoogleButton(),
+                            Padding(padding: EdgeInsets.all(10)),
+                            LoginWithFacebookButton()
+                          ],
+                        ),
+                      ]),
+                )),
+          ]),
+    );
   }
 }
