e5c0c18600b8d66b3c6e7be1907d588ced5d3727
davidalexandru1370 <fanefaraon696@gmail.com>
2023-08-31 22:20:55 +0300

    added: presentation layer

---
+++ backend/src/main/presentation/src/test/java/backend/presentation/PresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.presentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
