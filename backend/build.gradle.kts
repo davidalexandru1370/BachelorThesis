07c9014ae448482ffed1b8b8dec6a632ce1e54e6
davidalexandru1370 <fanefaraon696@gmail.com>
2023-09-11 21:15:35 +0300

    added: integration of liquibase

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -20,6 +20,17 @@ repositories {
     mavenCentral()
 }
 
+configurations {
+    liquibase {
+        activities.register("main")
+        runList = "main"
+    }
+
+    liquibaseRuntime {
+        extendsFrom(project.configurations.compileClasspath.get())
+    }
+}
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
@@ -31,15 +42,22 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.modelmapper:modelmapper:3.1.1")
     implementation("com.h2database:h2:2.1.214")
-    implementation("org.liquibase:liquibase-core:4.22.0")
     implementation("org.jooq:jooq:3.18.6")
     runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("org.liquibase.ext:liquibase-hibernate6:4.22.0")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
     implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("org.liquibase.ext:liquibase-hibernate6:4.22.0")
+    liquibaseRuntime("org.liquibase:liquibase-core:4.22.0")
+    liquibaseRuntime("org.liquibase:liquibase-groovy-dsl:3.0.3")
+    liquibaseRuntime("org.postgresql:postgresql")
+    liquibaseRuntime("org.liquibase.ext:liquibase-hibernate6:4.22.0")
+    liquibaseRuntime("info.picocli:picocli:4.7.4")
+    liquibaseRuntime(sourceSets.getByName("main").compileClasspath)
+    liquibaseRuntime(sourceSets.getByName("main").runtimeClasspath)
+    liquibaseRuntime(sourceSets.getByName("main").output)
 }
 
 tasks.withType<KotlinCompile> {
