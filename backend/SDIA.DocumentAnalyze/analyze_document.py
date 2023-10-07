b987652189bd53f6c3ee31a35eb6963d00f21de6
David Alexandru <davidalexandru1370@gmail.com>
2023-10-07 21:27:59 +0300

    added(SDIA.DocumentAnalysis): created requirements.txt

---
+++ backend/SDIA.DocumentAnalyze/analyze_document.py
@@ -0,0 +1,18 @@
+import cv2
+
+# Load the image
+img = cv2.imread("document.jpg")
+
+# Convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Apply thresholding to create a binary image
+thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# Find contours in the binary image
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# Draw the contours on the original image
+cv2.drawContours(img, contours, -1, (0, 0, 255), 2)
+
+# Display the image
