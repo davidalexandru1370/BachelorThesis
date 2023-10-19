f8decd8f3456d2ec5b0b3a838f0e57477548e4f0
David Alexandru <fanefaraon696@gmail.com>
2023-10-19 23:05:02 +0300

    added(app): populated document service details in appsettings.Development.json

--- backend/SDIA/Application/Configurations/DocumentServiceConfiguration.cs
+++ backend/SDIA/Application/Configurations/DocumentServiceConfiguration.cs
@@ -2,5 +2,9 @@ namespace Application.Services;
 
 public class DocumentServiceConfiguration
 {
-    
+    public string BaseUrl { get; set; }
+    public int Port { get; set; }
+    public string AnalyseDocumentEndpoint { get; set; }
+
+    public string GetAnalyseDocumentEndpoint => $"{BaseUrl}:{Port}{AnalyseDocumentEndpoint}";
 }
