661f2772c93c97a0d4746b6c5d40802b75ff56a6
David Alexandru <fanefaraon696@gmail.com>
2023-10-21 19:00:53 +0300

    added(SDIA.DocumentAnalyze): analyze document concrete implementation

--- backend/SDIA.DocumentAnalyze/analyze_document.py
+++ backend/SDIA.DocumentAnalyze/analyze_document.py
@@ -1,23 +1,47 @@
 import cv2
-from flask import Flask, request
-
-# Load the image
-img = cv2.imread("document.jpg")
-
-# Convert to grayscale
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-# Apply thresholding to create a binary image
-thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-# Find contours in the binary image
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# Draw the contours on the original image
-cv2.drawContours(img, contours, -1, (0, 0, 255), 2)
-
-# Display the image
+import time
+import os
+import math
+import numpy as np
+import pytesseract
 
 
+class AnalyzeDocument:
+    def __init__(self):
+        self.__execute_document_scanning()
+
+    def __execute_document_scanning(self):
+        # Load the image
+        img = cv2.imread(r"resources\documents\buletin.PNG", 0)
+
+        img_copy = img.copy()
+
+        img_canny = cv2.Canny(img_copy, 50, 100, apertureSize=3)
+
+        img_hough = cv2.HoughLinesP(img_canny, 1, math.pi / 180, 100, minLineLength=100, maxLineGap=10)
+
+        (x, y, w, h) = (np.amin(img_hough, axis=0)[0, 0], np.amin(img_hough, axis=0)[0, 1],
+                        np.amax(img_hough, axis=0)[0, 0] - np.amin(img_hough, axis=0)[0, 0],
+                        np.amax(img_hough, axis=0)[0, 1] - np.amin(img_hough, axis=0)[0, 1])
+
+        img_roi = img_copy[y:y + h, x:x + w]
+
+        (height, width) = img_roi.shape
+        img_roi_copy = img_roi.copy()
+        dim_mrz = (x, y, w, h) = (1, round(height * 0.9), width - 3, round(height - (height * 0.9)) - 2)
+        img_roi_copy = cv2.rectangle(img_roi_copy, (x, y), (x + w, y + h), (0, 0, 0), 2)
+
+        img_mrz = img_roi[y:y + h, x:x + w]
+        img_mrz = cv2.GaussianBlur(img_mrz, (3, 3), 0)
+        ret, img_mrz = cv2.threshold(img_mrz, 127, 255, cv2.THRESH_TOZERO)
+
+        if os.name == "nt":
+            pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+        mrz = pytesseract.image_to_string(img_mrz, config='--psm 12')
+        
+        cv2.imshow("da", img_roi)
 
 
+if __name__ == "__main__":
+    analyzeDocument: AnalyzeDocument = AnalyzeDocument()
