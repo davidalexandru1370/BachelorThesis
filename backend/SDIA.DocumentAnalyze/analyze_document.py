35551d7af61a6b79a74cc8decce0ed7bec902e4f
David Alexandru <fanefaraon696@gmail.com>
2023-11-08 23:32:42 +0200

    added: fix frontend on windows

--- backend/SDIA.DocumentAnalyze/analyze_document.py
+++ backend/SDIA.DocumentAnalyze/analyze_document.py
@@ -1,46 +1,54 @@
 import cv2
-import time
 import os
 import math
+import time
 import numpy as np
 import pytesseract
+
+if os.name == "nt":
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 
 class AnalyzeDocument:
     def __init__(self):
-        self.__execute_document_scanning()
+        self.execute_document_scanning()
 
-    def __execute_document_scanning(self):
+    def execute_document_scanning(self):
         # Load the image
-        img = cv2.imread(r"resources\documents\buletin.PNG", 0)
+        img = cv2.imread(r"resources\documents\buletin.PNG")
 
-        img_copy = img.copy()
+        # Convert the image to gray scale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        #cv2.imshow("da", gray)
+        cv2.waitKey(0)
 
-        img_canny = cv2.Canny(img_copy, 50, 100, apertureSize=3)
+        # Performing OTSU threshold
+        ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
 
-        img_hough = cv2.HoughLinesP(img_canny, 1, math.pi / 180, 100, minLineLength=100, maxLineGap=10)
+        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (100, 25))
 
-        (x, y, w, h) = (np.amin(img_hough, axis=0)[0, 0], np.amin(img_hough, axis=0)[0, 1],
-                        np.amax(img_hough, axis=0)[0, 0] - np.amin(img_hough, axis=0)[0, 0],
-                        np.amax(img_hough, axis=0)[0, 1] - np.amin(img_hough, axis=0)[0, 1])
+        # Applying dilation on the threshold image
+        dilation = cv2.dilate(thresh1, rect_kernel, iterations=1)
 
-        img_roi = img_copy[y:y + h, x:x + w]
+        # Finding contours
+        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
+                                               cv2.CHAIN_APPROX_NONE)
 
-        (height, width) = img_roi.shape
-        img_roi_copy = img_roi.copy()
-        dim_mrz = (x, y, w, h) = (1, round(height * 0.9), width - 3, round(height - (height * 0.9)) - 2)
-        img_roi_copy = cv2.rectangle(img_roi_copy, (x, y), (x + w, y + h), (0, 0, 0), 2)
+        im2 = img.copy()
 
-        img_mrz = img_roi[y:y + h, x:x + w]
-        img_mrz = cv2.GaussianBlur(img_mrz, (3, 3), 0)
-        ret, img_mrz = cv2.threshold(img_mrz, 127, 255, cv2.THRESH_TOZERO)
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
 
-        if os.name == "nt":
-            pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+            # Drawing a rectangle on copied image
+            rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        mrz = pytesseract.image_to_string(img_mrz, config='--psm 12')
-        
-        cv2.imshow("da", img_roi)
+            # Cropping the text block for giving input to OCR
+            cropped = im2[y:y + h, x:x + w]
+
+            # Apply OCR on the cropped image
+            text = pytesseract.image_to_string(cropped)
+            if len(text.strip()) > 0:
+                print(text)
 
 
 if __name__ == "__main__":
